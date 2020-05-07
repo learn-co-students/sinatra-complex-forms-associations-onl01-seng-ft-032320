5693ea640ecf596454eaf5f918b64758f5077040
BenWix <benwichser@gmail.com>
2020-05-07 10:07:44 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,22 +5,46 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all 
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    #binding.pry
+    @pet = Pet.create(name: params[:pet_name])
+    if params[:owner][:name] == ""
+      @pet.owner = Owner.find(params[:owner_id])
+    else 
+      @pet.owner = Owner.create(params[:owner])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
-    @pet = Pet.find(params[:id])
+    @pet = Pet.find_by_id(params[:id])
+    #binding.pry
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do 
+    @pet = Pet.find_by_id(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit' 
+  end
 
+  patch '/pets/:id' do 
+    @pet = Pet.find_by_id(params[:id])
+    
+    @pet.update(name: params[:pet_name])
+   #binding.pry
+    if params[:owner][:name] == ""
+      @pet.owner = Owner.find_by_id(params[:owner_id])
+    else 
+      @pet.owner = Owner.create(params[:owner])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
