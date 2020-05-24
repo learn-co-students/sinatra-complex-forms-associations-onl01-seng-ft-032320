064fbb8a8b18f77b8e4ad716041097cc90f4ad93
Nicholas Keller <nic.keller328@gmail.com>
2020-05-24 21:31:56 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -19,12 +19,12 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/:id/edit' do
-    @owner = Owner.find(params[:id])
+    @owner = Owner.find_by_id(params[:id])
     erb :'/owners/edit'
   end
 
   get '/owners/:id' do
-    @owner = Owner.find(params[:id])
+    @owner = Owner.find_by_id(params[:id])
     erb :'/owners/show'
   end
 
@@ -35,7 +35,7 @@ class OwnersController < ApplicationController
     end
     #######
 
-    @owner = Owner.find(params[:id])
+    @owner = Owner.find_by_id(params[:id])
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
